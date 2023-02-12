ca89bba2f5363fd124b12de3caa3a1767200d2e9
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-12 13:45:02 +0300

    add Задача 4

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -1,11 +1,34 @@
-﻿// Задача. Программа, которая на вход принимает число N, а выводит множество от -N до N
-int.TryParse(Console.ReadLine(), out int number); // вводим число
+﻿bool isWork = true;
 
-var negativN = number * (-1); // умножаем на (-1)
-
-while (negativN <= number)
+while(isWork)
 {
-  Console.Write(negativN + " ");
-  negativN++;
-}
+  // Console.Write("Введите номер задачи: ");
+  // if (int.TryParse(Console.ReadLine(), out int i))
+  
+  //   switch(i)
+  //   {
+  //     case 1:
+  //     {
+        // Задача. Программа, которая на вход принимает число N, а выводит множество от -N до N
+        int.TryParse(Console.ReadLine(), out int number); // вводим число
 
+        var negativN = number * (-1); // умножаем на (-1)
+
+        while (negativN <= number)
+        {
+          Console.Write(negativN + " ");
+          negativN++;
+        }
+      // }
+      // case 2:
+      // {
+          ///////////////////
+          // Задача 2
+
+        int.TryParse(Console.ReadLine(), out int N);
+
+        Console.WriteLine(N % 10);
+//       }
+//     }
+// }
+ 
